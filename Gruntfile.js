728b53e684b09be8112fb124b61964f1ae5b881b
Gabriel Cavallo <gabrielcavallo@mail.com>
2016-09-04 04:18:57 -0400

    Fix pt and zh file rename

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,6 +89,8 @@ module.exports = function (grunt) {
           dot: true,
           src: [
             '.tmp',
+            '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-pt-BR.json',
+            '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-zh-CN.json',
             '<%= config.app %>/scripts/background2.js'
           ]
         }]
@@ -194,9 +196,9 @@ module.exports = function (grunt) {
           '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-es.json': '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-es.json',
           '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-fr.json': '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-fr.json',
           '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-ja.json': '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-ja.json',
-          '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-pt-BR.json': '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-pt.json',
+          '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-pt.json': '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-pt-BR.json',
           '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-ru.json': '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-ru.json',
-          '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-zh-CN.json': '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-zh.json'
+          '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-zh.json': '<%= config.dist %>/geolite2/GeoLite2-Country-Locations-zh-CN.json'
         }
       }
     },
